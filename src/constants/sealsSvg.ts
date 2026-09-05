4c6ad4bb83ba337a6be7e13a163b51c5a11de81d
Freda <fredapacitan@gmail.com>
2026-09-05 23:46:12 +0700

    design: add sealsSvg.ts with scalloped gear borders and embossed medal icons

---
+++ src/constants/sealsSvg.ts
@@ -0,0 +1,6 @@
+/**
+ * Official Seal Vector Templates
+ */
+
+export const GOLD_MEDAL_STAR_SVG = `<polygon points="50,15 61,38 85,38 66,53 73,76 50,62 27,76 34,53 15,38 39,38" fill="currentColor"/>`;
+export const ACADEMIC_CAP_SVG = `<path d="M10 35 L 50 15 L 90 35 L 50 55 Z M 50 55 L 50 75 M 25 43 L 25 65 C 25 75, 75 75, 75 65 L 75 43" stroke="currentColor" stroke-width="4" fill="none"/>`;
