3f7d19d160a13beb581b76f87310480c724316d4
Freda <fredapacitan@gmail.com>
2026-09-05 23:46:00 +0700

    types: define project folder organization and metadata schemas

---
+++ src/types/project.ts
@@ -0,0 +1,11 @@
+/**
+ * Project Workspace Schema Types
+ */
+
+export interface ProjectMetadata {
+  title: string;
+  organization: string;
+  category: string;
+  tags: string[];
+  lastModifiedBy: string;
+}
